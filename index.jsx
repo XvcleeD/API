8d7268e8ef601130ae694ce257e5ce3e272a4612
XvcleeD <Xvclee255@gmail.com>
2023-01-26 18:46:16 +0800

    huulab

--- index.jsx
+++ index.jsx
@@ -1,40 +1,91 @@
 const express = require("express");
 const cors = require("cors")
+const { v4: uuid } =require("uuid");
+// const { response } = require("express");
 
-const port = 8000;
+const port = 3001;
 const app = express()
 
-const users = [
+app.use(cors());
+app.use(express.json());
+
+let categorios = [
+  // {
+  //   id: uuid(),
+  //   name: "Hen bilee"
+  // },
   {
-    id: 1,
-    name: "Huslee"
-  },
-  {
-    id: 2,
+    id: uuid(),
     name: "Henee"
   },
   {
-    id: 3,
+    id: uuid(),
     name: "Horloo"
   },
+];
 
-
-]
-
-app.use(cors());
-
-app.get("/", (request, response) => {
-  response.send("Hello")  
+app.get("categories", (require, response) =>{
+  response.json(categories);
 })
 
-app.get("/user", (require, response)=>{
-  response.json(users);
+app.get("/categories/:id", (require, response) =>{
+  const {id} = require.params;
+  const one = categories.find((category) => category.id === id);
+  if (one){
+    response.json(one)
+  }else{
+    response.sendStatus(404);
+  }
+});
+
+
+app.post("/categories", (require, response)=>{
+  const { name } = require.body;
+  const newCategory = { id: uuid(), nema: name};
+  categorios.push(newCategory);
+  response.sendStatus(201);
 })
 
-app.get("/user/save", (require, response)=>{
-  users.push({name: "Nairaan baian"})
-  response.json(users)
-})
+// app.delete("categories/:id", (require, response)=>{
+//   const { id } = require.params;
+//   const one = categorios.find((category) => category.id === id);
+//   if(one){
+//     const newList = categorios.filter((category) => category.id !== id );
+//     categorios = newList; 
+//     response.json({deleteId: id});
+//   } else{ 
+//     response.sendStatus(404)
+//   }
+// });
+
+// app.put("/categories/:id", (require, response) => {
+//   const { id } = require.params;
+//   const { name } = require.body;
+//   const index = categorios.findIndex((category) => category.id === id);
+//   id (index > -1){
+//     categories[index].name = name;
+//     response.json({updatedId: id});
+//   } else{
+//     response.sendStatus(404);
+//   }
+// })
+
+
+// (require, response)
+// app.use(cors());
+
+// app.get("/", (request, response) => {
+//   response.send("Hello")  
+// })
+
+// app.get("/user", (require, response)=>{
+//   response.json(users);
+// })
+
+// app.get("/user/save", (require, response)=>{
+//   users.push({name: "Nairaan baian"})
+//   response.json(users)
+// })
 
 app.listen(port, () => {
   console.log("App is listering at port", port)
