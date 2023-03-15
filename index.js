73e2e9e97c523676d8f8aac160267bbd1f22aa85
XvcleeD <Xvclee255@gmail.com>
2023-03-15 18:50:22 +0800

    angid

--- index.js
+++ index.js
@@ -2,14 +2,23 @@ const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
 const { v4: uuid } = require("uuid");
-const axios = require("axios");
+// const axios = require("axios");
 const mysql = require("mysql2");
+const bcrypt = require("bcryptjs");
 const user = {
-  username: "Horolmaa",
-  password: "balgan",
+  username: "123",
+  password: "$2a$10$9mvLnNBgamGl5VQJtXIcM.BIlF6CXRdk5fADn9q9CEAhI98og5mH.",
 };
 
-let userTokens = [];
+// let pass = bcrypt.hashSync("mypassword");
+// console.log(pass);
+
+// let mypasshash = "$2a$10$9mvLnNBgamGl5VQJtXIcM.BIlF6CXRdk5fADn9q9CEAhI98og5mH.";
+
+// console.log(bcrypt.compareSync("mypassword", mypasshash));
+
+// console.log(userTokens);
+// let userTokens = [];
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -22,6 +31,12 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+function readUserToken() {
+  const content = fs.readFileSync("token.json");
+  const userToken = JSON.parse(content);
+  return userToken;
+}
 
 function readCategories() {
   const content = fs.readFileSync("categories.json");
@@ -41,10 +56,15 @@ function readArticles() {
 // }
 app.get("/login", (req, res) => {
   const { username, password } = req.query;
-
-  if (user.username === username && user.password === password) {
+  // console.log(req);
+  if (
+    user.username === username &&
+    bcrypt.compareSync(password, user.password)
+  ) {
     const token = uuid();
-    userTokens.push(token);
+    const userToken = readUserToken();
+    userToken.push(token);
+    fs.writeFileSync("token.json", JSON.stringify(userToken));
     res.json({ token });
   } else {
     res.sendStatus(401);
@@ -62,7 +82,14 @@ app.get("/mysql-test", (req, res) => {
 });
 
 app.get("/categories", (req, res) => {
-  const { q } = req.query;
+  const { q, token } = req.query;
+
+  const userToken = readUserToken();
+
+  if (!userToken.includes(token)) {
+    res.sendStatus(401);
+    return;
+  }
   const categories = readCategories();
   if (q) {
     const filteredList = categories.filter((category) =>
@@ -277,7 +304,6 @@ app.get("/articles", (req, res) => {
 app.listen(port, () => {
   console.log("App is listering at port", port);
 });
-
 
 // Xvclee2551
 // Xvclee0316
