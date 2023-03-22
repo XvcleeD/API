ad48604f4ea58c7b1bbaf5f1734bfa3c80d5219a
XvcleeD <Xvclee255@gmail.com>
2023-03-22 20:08:42 +0800

    angid

--- index.js
+++ index.js
@@ -9,16 +9,17 @@ const mongoose = require("mongoose");
 const multer = require("multer");
 const { config } = require("process");
 const cloudinary = require("cloudinary");
+require("dotenv").config();
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
-})
+});
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "/tmp");
+    cb(null, "/tmp/");
   },
   filename: function (req, file, cb) {
     const extentsions = file.originalname.split(".").pop();
@@ -29,11 +30,8 @@ const upload = multer({
   storage: storage,
 });
 
-
 mongoose
-  .connect(
-    "mongodb+srv://Xvclee2551:Xvclee0316@cluster0.zso2y9y.mongodb.net/test"
-  )
+  .connect(process.env.MONGODB_STRING)
   .then(() => console.log("Connected!"));
 
 const port = 3001;
@@ -47,30 +45,33 @@ const userSchema = new mongoose.Schema({
   // birthDate: String
 });
 
-const User = mongoose.model("User", userSchema, "hereglegch");
-
 app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
-app.post("/upload-image", upload.single("image"),async function (req, res, next) {
-  const upload = await cloudinary.v2.uploader.uploud(req.file.path);
-  res.json({
-    success: true,
-    file: upload.secure_url,
-  });
-});
+app.post(
+  "/upload-image",
+  upload.single("image"),
+  async function (req, res, next) {
+    const cloudinaryImage = await cloudinary.v2.uploader.upload(req.file.path);
+    return res.json({
+      path: cloudinaryImage.secure_url,
+        width: cloudinaryImage.width,
+        height: cloudinaryImage.height,
+    });
+  }
+);
 app.use("/categories", categoryRouter);
 app.use("/articles", articleRouter);
-app.get("/test-mongoose", (req, res) => {
-  User.create({
-    name: "Baldan",
-    email: "baldan@horl.mn",
-    age: 18,
-    createdAt: new Date(),
-  });
+// app.get("/test-mongoose", (req, res) => {
+//   User.create({
+//     name: "Baldan",
+//     email: "baldan@horl.mn",
+//     age: 18,
+//     createdAt: new Date(),
+//   });
 
-  res.json({});
-});
+//   res.json({});
+// });
 
 app.listen(port, () => {
   console.log("App is listering at port", port);
