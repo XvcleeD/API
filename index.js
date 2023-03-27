076528022ed3d975c241b82ff8a2a220f5c9cf74
XvcleeD <Xvclee255@gmail.com>
2023-03-27 19:40:44 +0800

    register router

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const { categoryRouter } = require("./routes/categoryController");
 const { articleRouter } = require("./routes/articleController");
+const { userRouter } = require("./routes/userController");
 const bcrypt = require("bcryptjs");
-const fs = require("fs");
+// const fs = require("fs");
 const { v4: uuid } = require("uuid");
 const mongoose = require("mongoose");
 const multer = require("multer");
@@ -36,17 +37,10 @@ mongoose
 const port = 3001;
 const app = express();
 
-const userSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  age: Number,
-  createdAt: Date,
-  // birthDate: String
-});
-
 app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
+app.use("/users", userRouter);
 app.post(
   "/upload-image",
   upload.single("image"),
@@ -76,34 +70,34 @@ app.listen(port, () => {
   console.log("App is listering at port", port);
 });
 
-function readUsers() {
-  const content = fs.readFileSync("users.json");
-  const user = JSON.parse(content);
-  return user;
-}
-function readUserToken() {
-  const content = fs.readFileSync("token.json");
-  const userToken = JSON.parse(content);
-  return userToken;
-}
+// function readUsers() {
+//   const content = fs.readFileSync("users.json");
+//   const user = JSON.parse(content);
+//   return user;
+// }
+// function readUserToken() {
+//   const content = fs.readFileSync("token.json");
+//   const userToken = JSON.parse(content);
+//   return userToken;
+// }
 
-app.get("/login", (req, res) => {
-  const { username, password } = req.query;
-  const user = readUsers();
+// app.get("/login", (req, res) => {
+//   const { username, password } = req.query;
+//   const user = readUsers();
 
-  if (
-    user.username === username &&
-    bcrypt.compareSync(password, user.password)
-  ) {
-    const token = uuid();
-    const userToken = readUserToken();
-    userToken.push(token);
-    fs.writeFileSync("token.json", JSON.stringify(userToken));
-    res.json({ token });
-  } else {
-    res.sendStatus(401);
-  }
-});
+//   if (
+//     user.username === username &&
+//     bcrypt.compareSync(password, user.password)
+//   ) {
+//     const token = uuid();
+//     const userToken = readUserToken();
+//     userToken.push(token);
+//     fs.writeFileSync("token.json", JSON.stringify(userToken));
+//     res.json({ token });
+//   } else {
+//     res.sendStatus(401);
+//   }
+// });
 
 // Xvclee2551
 // Xvclee0316
